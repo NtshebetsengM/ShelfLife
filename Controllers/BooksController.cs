9423ddb752a2a6b4ebd04271cabebbeec039adc6
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2026-09-03 17:05:56 +0200

    Add Index, Details, and Create actions to BooksController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShelfLife.Data;
+using ShelfLife.Models;
+
+namespace ShelfLife.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var book = _context.Books
+                .FirstOrDefault(b => b.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));         
+        }
+    }
+}
